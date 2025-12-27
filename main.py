6dcd578b926a38108da4f3b20f5c1bab263f7b96
Mohit Saini <mohitsaini656565@gmail.com>
2025-12-27 20:48:07 +0530

    main.py

--- main.py
+++ main.py
@@ -3,31 +3,25 @@ from google import genai
 
 st.title("My First Chatbot 🤖")
 
-# Load API key from Streamlit Secrets
 client = genai.Client(api_key=st.secrets["GOOGLE_API_KEY"])
 
-# Chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Show old messages
 for msg in st.session_state.messages:
     with st.chat_message(msg["role"]):
         st.write(msg["content"])
 
-# User input
 user_input = st.chat_input("Type your message...")
 
 if user_input:
-    # Show user message
     st.session_state.messages.append({"role": "user", "content": user_input})
     with st.chat_message("user"):
         st.write(user_input)
 
-    # Gemini response
     response = client.models.generate_content(
-        model="gemini-1.5-flash",
-        contents=user_input
+        model="models/gemini-1.5-flash",
+        contents=[user_input]
     )
 
     reply = response.text
@@ -38,6 +32,7 @@ if user_input:
     st.session_state.messages.append(
         {"role": "assistant", "content": reply}
     )
+
 
 
 
